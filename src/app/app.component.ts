0effb1e06186aed36f5c023e8ec2caee963ca4e7
hone5t <hone5t@hone5t.com>
2017-08-11 06:30:05 -0500

    redirect user based on login status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,32 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import firebase  from 'firebase';
+import { Config } from './config'
+
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any ;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
+      // intialize firebase with configration
+      firebase.initializeApp(Config.Config);
+      const unsbscribe = firebase.auth().onAuthStateChanged( user => {
+        if (!user) {
+          this.rootPage = 'login';
+        } else {
+          this.rootPage = HomePage;
+        }
+      } );
       statusBar.styleDefault();
       splashScreen.hide();
     });
   }
+
+
 }
 
