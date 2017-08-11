09d2d1f700a4c4fae4ae2e45006919339b773641
hone5t <hone5t@hone5t.com>
2017-08-11 08:45:17 -0500

    setup login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController, Loading } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+import { HomePage } from '../home/home';
 
 /**
  * Generated class for the LoginPage page.
@@ -16,10 +19,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+  loading: Loading;
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public formBuilder: FormBuilder,
+              public loadingCtrl: LoadingController,
+              public alertCtrl: AlertController,
+              public authProivder: AuthProvider
+  ) {
+    this.loginForm = formBuilder.group({
+      email:['',Validators.compose([Validators.required])],
+      password:['',Validators.compose([Validators.required])]
+    })
   }
-
+  loginUser(): void{
+    if (!this.loginForm.valid) {
+      console.log(this.loginForm);
+    } else {
+      this.authProivder.loginUser(this.loginForm.value.email,this.loginForm.value.password)
+      .then( user => {
+        this.loading.dismiss().then( () =>{
+          this.navCtrl.setRoot(HomePage);
+        });
+      },error => {
+        this.loading.dismiss().then( ()=> {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons : [{
+            text: 'Ok',
+            role: 'cancel'
+            }]
+          });
+          alert.present();
+        });
+      });
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
+    }
+  }
+  goToSignup() {
+    this.navCtrl.push('signup');
+  }
+  goToResetpassword() {
+    this.navCtrl.push('reset-password');
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
