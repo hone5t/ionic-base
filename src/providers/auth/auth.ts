f77961e32de7cf6161fc10819d759667a5a1768b
hone5t <hone5t@hone5t.com>
2017-08-11 06:48:17 -0500

    compleate auth provider

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import firebase from 'firebase';
+import { Config } from '../../app/config';
+
 /*
   Generated class for the AuthProvider provider.
 
@@ -10,9 +13,35 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class AuthProvider {
+  public fireAuth: firebase.auth.Auth;
+  public userProfileRef: firebase.database.Reference;
 
   constructor(public http: Http) {
-    console.log('Hello AuthProvider Provider');
+    this.fireAuth = firebase.auth();
+    this.userProfileRef = firebase.database().ref(Config.UserProfileRef);
+  }
+
+  loginUser(email: string, password: string): firebase.Promise<any> {
+    return this.fireAuth.signInWithEmailAndPassword(email,password);
+  }
+
+  signupUser(email: string, password: string): firebase.Promise<any> {
+    return this.fireAuth.createUserWithEmailAndPassword(email,password)
+    .then( newUser => {
+      this.userProfileRef.child(newUser.uid).set({
+        email: email
+      });
+    });
+  }
+
+  resetPassword(email: string): firebase.Promise<void>{
+    return this.fireAuth.sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): firebase.Promise<void>{
+    // turn the reference off
+    this.userProfileRef.child(this.fireAuth.currentUser.uid).off();
+    return this.fireAuth.signOut();
   }
 
 }
